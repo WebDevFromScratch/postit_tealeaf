6c766fe5e7d50ad6de76f3e83d6ad490f7b3e77c
Piotr Klosinski <mybettingstuff@gmail.com>
2014-06-29 14:45:04 +0200

    added registering users feature; authorization of password (on login) - WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,13 @@ module ApplicationHelper
     obj.url.start_with?("http://") ? obj.url : "http://#{obj.url}"
   end
 
-  def pretty_date_time(obj)
-    "#{obj.created_at.to_formatted_s(:long)} UTC"
+  def pretty_date_time(dt)
+    #"#{dt.to_formatted_s(:long)} UTC" #this was my initial approach
+
+    dt.strftime("%m/%d/%Y %l:%M%P %Z") #this is just time formatting, syntax
+                                        #is given in the method docs
+                                        #(check Time in Ruby/Rails)
   end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
+
+  resources :users, only: [:create, :index]
+  #post '/users', to: 'users#create'
+  #get '/users', to: 'users#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :posts, except: [:destroy] do 
     resources :comments, only: [:create]
   end
