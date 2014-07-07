9ef5468a2a98e96e40d0df77c1a9acb73068ea8b
Piotr Klosinski <mybettingstuff@gmail.com>
2014-07-07 10:29:54 +0200

    fixed a bug on the user view page (crashing on comments tab)

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
   validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
-  #validates_uniqueness_of :user, scope: :voteable
+  #validates_uniqueness_of :user, scope: :voteable - this syntax is not supported
+                                                    #anymore
 end
