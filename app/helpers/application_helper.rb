76ff6dcf79d3290afb198390ee828987d508ddee
Piotr Klosinski <mybettingstuff@gmail.com>
2014-07-12 16:24:49 +0200

    added timezone choosing and displaying for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,10 @@ module ApplicationHelper
   end
 
   def pretty_date_time(dt)
+    if logged_in? && !current_user.timezone.blank?
+      dt = dt.in_time_zone(current_user.timezone)
+    end
     #"#{dt.to_formatted_s(:long)} UTC" #this was my initial approach
-
     dt.strftime("%m/%d/%Y %l:%M%P %Z") #this is just time formatting, syntax
                                         #is given in the method docs
                                         #(check Time in Ruby/Rails)
